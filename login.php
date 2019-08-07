de4c40e23d10dccdbc4c8d4e4bf2e8cb31b56229
Vishnu Pratap <47685528+vishnusinghp@users.noreply.github.com>
2019-08-07 18:25:51 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,14 @@
+<form method="post" id="login_check">
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email" name="email">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
+                        </div>
+                        <div class="text-right form-group">
+                            <a href="#"><small>Forgot password?</small></a>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="form-btn">Submit</button>
+                        </div>
+                    </form>
