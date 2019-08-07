8459abd6ef4815faa60db25b1339978e2ff26330
Vishnu Pratap <47685528+vishnusinghp@users.noreply.github.com>
2019-08-07 18:26:08 +0530

    Create register.php

---
+++ register.php
@@ -0,0 +1,17 @@
+<form method="post" name="register_form" id="register_form">
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="First Name *" name="first_name">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Last Name" name="Last_name">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email *" name="email">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password *" name="password">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="form-btn">Signup</button>
+                        </div>
+                    </form>
